2c70a9033277f112d50ec5af34fa0adbf5bb13a9
YetAnotherFurryMan <123496393+YetAnotherFurryMan@users.noreply.github.com>
2023-05-28 18:06:40 +0200

    Version Beth 2.4
    DataStructure renamed to ml (markup language)

--- DataStructure/src/yyerror.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void ds_yyerror(char* msg, char* yytext, int lineno){
-    fprintf(stderr, "At line %d:\n%s\n\tyytext:\'%s\'\n", lineno, msg, yytext);
-    exit(-1);
-}
