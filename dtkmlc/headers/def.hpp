afab918fc522cc8f4970dd1afb8f6d1181b13cb8
YetAnotherFurryMan <123496393+YetAnotherFurryMan@users.noreply.github.com>
2023-07-25 16:22:57 +0000

    Some new features and new "to do" list

--- dtkmlc/headers/def.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-typedef enum{
-    OUTPUT_TXT,
-    OUTPUT_XML
-} output_format;
-
-#define STRCMP_EQ(a, b) (strcmp(a, b) == 0)
--- dtkmlc/src/att_to_txt.cpp
+++
@@ -1,7 +0,0 @@
-#include "../headers/converters.hpp"
-
-#include <sstream>
-
-std::string att_to_txt(const dtk::ml::MLAttribute* a){
-    return std::string(a->name) + "=\"" + ((a->value)?(std::string(dtk::ml::ml_format_str(a->value))):"") + "\"";
-}
